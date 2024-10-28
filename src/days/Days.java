e5db1d523f39cae6070dfcee52c4efb7102ebcff
wenzhenw <wanwenzh@sheridancollege.ca>
2024-10-28 17:32:02 -0400

    days

--- src/days/Days.java
+++ src/days/Days.java
@@ -18,18 +18,15 @@ import java.util.Scanner;
  jn 22nd
  */
 public class Days {
-
+    enum Weekdays {
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         
-    Scanner in =new Scanner(System.in);
-    System.out.println("Enter the day number (one,two,three,four,five) in string");
-    String code = in.next();
-    Weekdays t= new Weekdays();
-    t.nameOfDay(code); 
-    }// TODO code application logic here
-    
-    
+         for (Weekdays day : Weekdays.values()) {
+             System.out.println(day);
+         }
+    }
 }
